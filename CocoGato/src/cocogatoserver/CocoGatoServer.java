a2ae779897a24be035be92ecc823dd005fe59e4c
Angel <padilla.angel88@gmail.com>
2020-05-20 11:32:09 -0500

    server

--- CocoGato/src/cocogatoserver/CocoGatoServer.java
+++ CocoGato/src/cocogatoserver/CocoGatoServer.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package cocogato;
+
+package cocogatoserver;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,10 +8,10 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class CocoGato {
+public class CocoGatoServer {
 
     public static void main(String[] args) throws Exception {
-        
+        System.out.println("Server");
     }
 }
 
