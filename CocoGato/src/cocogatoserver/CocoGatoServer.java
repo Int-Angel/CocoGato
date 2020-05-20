6b2f7f41e8e316be708f153d0c33cedfca22a065
Ricardo <ricardoglr664@gmail.com>
2020-05-20 13:38:08 -0500

    this

--- CocoGato/src/cocogatoserver/CocoGatoServer.java
+++ CocoGato/src/cocogatoserver/CocoGatoServer.java
@@ -2,17 +2,47 @@
 package cocogatoserver;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CocoGatoServer {
 
     public static void main(String[] args) throws Exception {
-        DB db = new DB();
-        System.out.println("Server");
+        ServerSocket server;
+        final int port = 471;
+        DataInputStream in;
+        DataOutputStream out;
+        Socket sc = null;
+        
+         try {
+            server = new ServerSocket(port);
+            
+            System.out.println("Server iniciado");
+            
+            while(true)
+            {
+                sc = server.accept();
+                
+                in = new DataInputStream(sc.getInputStream());
+                out = new DataOutputStream(sc.getOutputStream());
+                
+                String message = in.readUTF();
+                System.out.println(message);
+                out.writeUTF("Server: conexión exitosa.");
+                sc.close();
+                System.out.println("Cliente desconectado");
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(CocoGatoServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
 
