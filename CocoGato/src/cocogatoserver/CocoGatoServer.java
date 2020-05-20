7c910972d926dcd8ddde65d993f181d70b992ee6
Angel <padilla.angel88@gmail.com>
2020-05-20 11:33:18 -0500

    server

--- CocoGato/src/cocogatoserver/CocoGatoServer.java
+++ CocoGato/src/cocogatoserver/CocoGatoServer.java
@@ -8,12 +8,8 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-<<<<<<< HEAD:CocoGato/src/cocogatoserver/CocoGatoServer.java
 public class CocoGatoServer {
 
-=======
-public class CocoGato {
->>>>>>> e84dd4f2bea2c2f34b86e30a718deeefd068d9bf:CocoGato/src/cocogato/CocoGato.java
     public static void main(String[] args) throws Exception {
         System.out.println("Server");
     }
